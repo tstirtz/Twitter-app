49401c01ee2f412dcdb850fd79be4f770d9dde38
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-02-10 23:25:37 -0800

    Add functionality to function stubs

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 'use strict'
 
+const twitterUrl = 'https://api.twitter.com/1.1/search/tweets.json';
+
 function handleSearchButton(){
-
+$('.js-search-button').on('click', function(event){
+    event.preventDefault();
+    let searchTerm = $(this).prev().val();
+    let encodedSearch = encodeURI(searchTerm);
+    requestToTwitter(encodedSearch);
+});
 }
 
 
 
 
-function requestToTweeter(){
+function requestToTwitter(query){
+    const settings = {
+        q: query,
+        lang: "en",
+        dataType: 'json',
+        always: 'request finished'
+    }
 
+    $.get(twitterUrl, settings, renderRequestResults);
 }
 
 
 
 
-function renderRequestResults(){
-
+function renderRequestResults(results){
+    console.log(results);
 }
 
 
